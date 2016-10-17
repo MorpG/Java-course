2122763a1bd9b353fa52e39d5ab09f07699de97a
MorpG <agolovin@list.ru>
2016-10-18 00:23:31 +0300

    improve task32

--- chapter_001/task32/src/test/java/ru/agolovin/MaxTriangleSideTest.java
+++ chapter_001/task32/src/test/java/ru/agolovin/MaxTriangleSideTest.java
@@ -9,7 +9,12 @@ public class MaxTriangleSideTest {
 
     @Test
     public void whenMaxTriangleSideExistThenResult() {
-        double result = MaxTriangleSide.maxSide(5.0, 4.0, 10.0);
-        assertThat(result, is(10.0));
+        Point point1 = new Point(3, 5);
+        Point point2 = new Point(6, 1);
+        Point point3 = new Point(3, 1);
+        Triangle tr = new Triangle(point1, point2, point3);
+        tr.sideLength();
+        double result = MaxTriangleSide.maxSide(tr.ab, tr.ac, tr.bc);
+        assertThat(result, is(5.0));
     }
 }
