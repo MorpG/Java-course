cbe968a815ffa3392194e0433b87a13e2f4af24d
MorpG <agolovin@list.ru>
2016-10-17 23:25:28 +0300

    improve task32
    improve task32

--- chapter_001/task32/src/test/java/ru/agolovin/MaxTriangleSideTest.java
+++ chapter_001/task32/src/test/java/ru/agolovin/MaxTriangleSideTest.java
@@ -4,29 +4,12 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-import static org.hamcrest.number.IsCloseTo.*;
 
 public class MaxTriangleSideTest {
 
     @Test
-    public void whenMaxTriangleSideNotExistTheResult() {
-        Point point1 = new Point(9, 4);
-        Point point2 = new Point(4, 9);
-        Point point3 = new Point(6, 7);
-        Triangle triangle = new Triangle(point1, point2, point3);
-        MaxTriangleSide triangleSide = new MaxTriangleSide();
-        double result = triangleSide.maxSide(triangle);
-        assertThat(result, is(0.0));
-    }
-
-    @Test
-    public void whenMaxTriangleSideExistThenResultIs() {
-        Point point1 = new Point(1, 4);
-        Point point2 = new Point(4, 9);
-        Point point3 = new Point(6, 10);
-        Triangle triangle = new Triangle(point1, point2, point3);
-        MaxTriangleSide triangleSide = new MaxTriangleSide();
-        double result = triangleSide.maxSide(triangle);
-        assertThat(result, is(closeTo(7.81, 0.001)));
+    public void whenMaxTriangleSideExistThenResult() {
+        double result = MaxTriangleSide.maxSide(5.0, 4.0, 10.0);
+        assertThat(result, is(10.0));
     }
 }
