18abb131a45845cf6ea8abe808d90d58a1d75007
MorpG <agolovin@list.ru>
2016-12-17 23:09:35 +0300

    add pre task 7.2

---
+++ chapter_002/Chess/src/main/java/ru/agolovin/models/Bishop.java
@@ -0,0 +1,66 @@
+package ru.agolovin.models;
+
+/**
+ * Class for chess figure Bishop.
+ *
+ * @author agolovin (agolovin@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Bishop extends Figure {
+
+    /**
+     * constructor.
+     * @param cell cell
+     */
+    public Bishop(final Cell cell) {
+        super(cell);
+    }
+
+    /**
+     * clone figure to dist cell.
+     * @param dist cell
+     * @return new figure.
+     */
+    @Override
+    public final Bishop clone(final Cell dist) {
+        return new Bishop(dist);
+    }
+
+    /**
+     * array of move cells.
+     * @param dist Cell
+     * @return array of cells
+     * @throws ImpossibleMoveException error
+     */
+    @Override
+    public final Cell[] way(final Cell dist) throws ImpossibleMoveException {
+        int startCol = this.position.getCol();
+        int startRow = this.position.getRow();
+        int endCol = dist.getCol();
+        int endRow = dist.getRow();
+        int maxSize = Math.abs(endCol - startCol);
+
+        int step = 1;
+
+        Cell[] res = new Cell[maxSize];
+
+        if ((Math.abs(endCol - startCol) == Math.abs(endRow - startRow))
+                && ((endCol - startCol) != 0) && (endRow - startRow) != 0) {
+            if (startCol > endCol) {
+                step = -1;
+            }
+
+            for (int i = 1; i <= maxSize; i++) {
+                int stepCol = startCol + step * i;
+                int stepRow = (stepCol - startCol) * (endRow - startRow)
+                        / (endCol - startCol) + startRow;
+                res[i - 1] = new Cell(startCol, stepRow);
+            }
+        } else {
+            throw new ImpossibleMoveException("Error: impossible move");
+        }
+
+        return res;
+    }
+}
