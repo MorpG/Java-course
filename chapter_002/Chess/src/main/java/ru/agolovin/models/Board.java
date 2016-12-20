9965da9aadaf75c7dadde36abb834244d4e5f2b7
MorpG <agolovin@list.ru>
2016-12-20 17:04:48 +0300

    fix task 7.2

--- chapter_002/Chess/src/main/java/ru/agolovin/models/Board.java
+++ chapter_002/Chess/src/main/java/ru/agolovin/models/Board.java
@@ -21,6 +21,7 @@ class Board {
 
     /**
      * Set figure to the board.
+     *
      * @param figure Figure
      */
     void setFigure(Figure figure) {
@@ -42,47 +43,44 @@ class Board {
     final boolean move(final Cell start, final Cell end)
             throws ImpossibleMoveException,
             FigureNotFoundException, OccupiedWayException {
-        boolean result = false;
+        boolean result = true;
         final int maxIndex = 7;
 
-           int startCol = start.getCol();
-            int startRow = start.getRow();
-            if (startCol < 0 || startRow < 0
-                    || startCol > maxIndex
-                    || startRow > maxIndex
-                    || (board[startCol][startRow] == null)) {
-                throw new FigureNotFoundException(
-                        "Error: Figure not found on the board");
+        int startCol = start.getCol();
+        int startRow = start.getRow();
+        if (startCol < 0 || startRow < 0
+                || startCol > maxIndex
+                || startRow > maxIndex
+                || (board[startCol][startRow] == null)) {
+            throw new FigureNotFoundException(
+                    "Error: Figure not found on the board");
+        }
+
+        if (end.getCol() < 0
+                || end.getCol() > maxIndex
+                || end.getRow() < 0
+                || end.getRow() > maxIndex) {
+            throw new ImpossibleMoveException(
+                    "Error: Destination out of the board");
+        }
+
+        Cell[] way = board[startCol][startRow].way(end);
+
+        for (Cell element : way) {
+            if (element == null) {
+                break;
             }
 
-            if (end.getCol() < 0
-                    || end.getCol() > maxIndex
-                    || end.getRow() < 0
-                    || end.getRow() > maxIndex) {
-                throw new ImpossibleMoveException(
-                        "Error: Destination out of the board");
+            if (board[element.getCol()][element.getRow()] != null) {
+                throw new OccupiedWayException(
+                        "Another figure on the way");
             }
+        }
 
-            Cell[] way = board[startCol][startRow].way(end);
+        board[end.getCol()][end.getCol()] =
+                board[startCol][startRow].clone(end);
 
-            for (Cell element : way) {
-                if (element == null) {
-                    break;
-                }
-
-                if (board[element.getCol()][element.getRow()] != null) {
-                    throw new OccupiedWayException(
-                            "Another figure on the way");
-                }
-            }
-
-            board[end.getCol()][end.getCol()] =
-                    board[startCol][startRow].clone(end);
-
-            board[startCol][startRow] = null;
-
-            result = true;
-
+        board[startCol][startRow] = null;
 
         return result;
     }
