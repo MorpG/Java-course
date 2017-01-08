e06467c485e6a28f8647c8a37b0a520de5d96e65
MorpG <agolovin@list.ru>
2017-01-09 00:35:27 +0300

    add pre task 3

--- chapter_003/task3/src/main/java/ru/agolovin/FileSort.java
+++ chapter_003/task3/src/main/java/ru/agolovin/FileSort.java
@@ -1,6 +1,5 @@
 package ru.agolovin;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -14,24 +13,65 @@ import java.io.RandomAccessFile;
  */
 public class FileSort {
 
+    private RandomAccessFile rafSource;
+
+    private RandomAccessFile rafDistance;
+
     private String lineSeparator = System.getProperty("lineSeparator");
 
     public void sort(File source, File distance) throws IOException {
+        String fileDistance = "distance.txt";
         try {
-            RandomAccessFile rafSource = new RandomAccessFile(source, "r");
+            checkExistFile(source);
+
+            distance = new File(fileDistance);
             RandomAccessFile rafDistance = new RandomAccessFile(distance, "rw");
 
-            String line = rafSource.readLine();
-            if (line != null) {
-                rafDistance.writeBytes(line);
-            }
-            while ((line = rafSource.readLine()) != null) {
-                rafDistance.writeBytes(String.format("%s%s", lineSeparator, line));
-            }
-        }
-        catch (IOException ioe) {
+            copyToDistance(source, distance);
+
+
+            File tempOne = File.createTempFile("tempOne", ".tmp");
+            File tempTwo = File.createTempFile("tempTwo", ".tmp");
+
+            RandomAccessFile rafTempOne = new RandomAccessFile(tempOne, "rw");
+            RandomAccessFile rafTempTwo = new RandomAccessFile(tempTwo, "rw");
+
+            boolean flag = true;
+            do {
+                if (splitSourceFile() == 1){
+                    flag = false;
+                }
+                merge();
+            } while (flag);
+
+        } catch (IOException ioe) {
             System.err.println("ioe");
 
         }
     }
+
+    void checkExistFile (File file) throws IOException{
+        if (file.exists() && file.isFile()) {
+            throw new IOException("source file invalid");
+        }
+    }
+
+    public int splitSourceFile(File source) {
+
+    }
+
+    private void copyToDistance(File source, File distance) throws IOException{
+        rafSource = new RandomAccessFile(source, "r");
+        rafDistance = new RandomAccessFile(distance, "rw");
+        String line = rafSource.readLine();
+        if (line != null) {
+            rafDistance.writeBytes(line);
+        }
+        while ((line = rafSource.readLine()) != null) {
+            rafDistance.writeBytes(String.format("%s%s", lineSeparator, line));
+        }
+        rafSource.close();
+    }
+
+
 }
