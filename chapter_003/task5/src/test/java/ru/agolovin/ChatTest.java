d36356e098feac080c90bc71ab590e22eb86a4b2
MorpG <agolovin@list.ru>
2017-01-17 18:17:55 +0300

    add task 3.1.5

--- chapter_003/task5/src/test/java/ru/agolovin/ChatTest.java
+++ chapter_003/task5/src/test/java/ru/agolovin/ChatTest.java
@@ -2,8 +2,8 @@ package ru.agolovin;
 
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -20,13 +20,30 @@ public class ChatTest {
      * @throws Exception exception
      */
     @Test
-    public void chat() throws Exception {
+    public void whenCheckSomeWordFromChatThenCheckForUserAndDictionary() throws Exception {
         String[] answer = {"First", "Стоп", "Word", "Продолжить", "Word2", "Закончить"};
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
         Chat chat = new Chat(new StubInput(answer));
         chat.chat();
 
-        boolean result = true;
-        assertThat(result, is(true));
+        boolean check1 = false;
+        boolean check2 = false;
+        boolean check3 = false;
+
+        if (out.toString().contains("Пользователь")) {
+            check1 = true;
+        }
+        if (out.toString().contains("Computer")) {
+            check2 = true;
+        }
+        if (out.toString().contains("Закончить")) {
+            check3 = true;
+        }
+
+        assertThat(check1, is(true));
+        assertThat(check2, is(true));
+        assertThat(check3, is(true));
 
     }
 }
