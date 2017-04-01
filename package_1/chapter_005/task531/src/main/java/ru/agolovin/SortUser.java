7ac564f561aa29d7fd07e34a7ae3e3fa42e87005
MorpG <agolovin@list.ru>
2017-04-01 22:08:13 +0300

    add task2 in part3 chapter5 package1

--- package_1/chapter_005/task531/src/main/java/ru/agolovin/SortUser.java
+++ package_1/chapter_005/task531/src/main/java/ru/agolovin/SortUser.java
@@ -1,5 +1,7 @@
 package ru.agolovin;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -10,6 +12,12 @@ import java.util.TreeSet;
  * В классе SortUser написать метод public Set<User> sort (List<User>),
  * который будет возвращать TreeSet пользователей, отсортированных
  * по возрасту в порядке возрастания.
+ * <p>
+ * В классе SortUser из 1-го задания необходимо написать два метода:
+ * public List<User> sortHash (List<User>) - в этом методе необходимо определить
+ * Comparator для метода Collections.sort и отсортировать List<User> по hash-коду.
+ * public List<User> sortLength (List<User>) - в этом методе необходимо определить
+ * Comparator для метода Collections.sort и отсортировать List<User> по длине имени.
  *
  * @author agolovin (agolovin@list.ru)
  * @version $Id$
@@ -28,6 +36,38 @@ public class SortUser {
         Set<User> set = new TreeSet<>();
         set.addAll(list);
         return set;
+    }
+
+    /**
+     * Sort List<User> by hashCode.
+     *
+     * @param list list
+     * @return List<User>
+     */
+    public List<User> sortHash(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.hashCode() - o2.hashCode();
+            }
+        });
+        return list;
+    }
+
+    /**
+     * Sort List<User> by name length.
+     *
+     * @param list list
+     * @return List<User>
+     */
+    public List<User> sortLength(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return list;
     }
 
 }
--- package_1/chapter_005/task531/src/test/java/ru/agolovin/SortUserTest.java
+++ package_1/chapter_005/task531/src/test/java/ru/agolovin/SortUserTest.java
@@ -7,8 +7,8 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author agolovin (agolovin@list.ru)
@@ -19,20 +19,47 @@ import static org.hamcrest.core.Is.is;
 public class SortUserTest {
 
     /**
+     * User 1.
+     */
+    private User user1 = new User("Name11", 2);
+
+    /**
+     * User 2.
+     */
+    private User user2 = new User("Name", 1);
+
+    /**
+     * User 3.
+     */
+    private User user3 = new User("Name333", 0);
+
+    /**
+     * Test class.
+     */
+    private SortUser sortUser = new SortUser();
+
+    /**
+     * List users.
+     */
+    private List<User> users = new ArrayList<>();
+
+    /**
+     * Add users to list.
+     * @param list list.
+     */
+    private void collectionsAdd(List<User> list) {
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+    }
+
+    /**
      * Sort user by age.
      */
     @Test
     public void whenAddSomeUserToListThenGiveSortedTreeMap() {
-        User user1 = new User("Name1", 2);
-        User user2 = new User("Name2", 1);
-        User user3 = new User("Name3", 0);
+        collectionsAdd(users);
 
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-
-        SortUser sortUser = new SortUser();
         Set<User> result = sortUser.sort(users);
 
         Set<User> answer = new LinkedHashSet<>();
@@ -40,6 +67,53 @@ public class SortUserTest {
         answer.add(user2);
         answer.add(user1);
 
+        assertThat(result, is(answer));
+    }
+
+    /**
+     * Sort List<Users> by hashCode.
+     */
+    @Test
+    public void whenAddSomeUserToListThenSortItByHashCode() {
+        collectionsAdd(users);
+        List<User> result = sortUser.sortHash(users);
+
+        List<User> answer = new ArrayList<>();
+        collectionsAdd(answer);
+
+        for (int i = 0; i < answer.size() - 1; i++) {
+            for (int j = i + 1; j < answer.size(); j++) {
+                if (answer.get(i).hashCode() > answer.get(j).hashCode()) {
+                    User temp = answer.get(i);
+                    answer.set(i, answer.get(j));
+                    answer.set(j, temp);
+                }
+            }
+        }
+
+        assertThat(result, is(answer));
+    }
+
+    /**
+     * Sort List<Users> by Name length.
+     */
+    @Test
+    public void whenAddSomeUserToListThenSortItByNameLength() {
+        collectionsAdd(users);
+        List<User> result = sortUser.sortLength(users);
+
+        List<User> answer = new ArrayList<>();
+        collectionsAdd(answer);
+
+        for (int i = 0; i < answer.size() - 1; i++) {
+            for (int j = i + 1; j < answer.size(); j++) {
+                if (answer.get(i).getName().length() > answer.get(j).getName().length()) {
+                    User temp = answer.get(i);
+                    answer.set(i, answer.get(j));
+                    answer.set(j, temp);
+                }
+            }
+        }
         assertThat(result, is(answer));
     }
 }
