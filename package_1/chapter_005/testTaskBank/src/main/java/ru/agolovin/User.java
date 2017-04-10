60882804f7265f65b15f826fcce749a6bec0c7fb
Aleksandr Golovin <agolovin@list.ru>
2017-04-10 08:52:03 +0300

    fixed TestTask in chapter5

--- package_1/chapter_005/testTaskBank/src/main/java/ru/agolovin/User.java
+++ package_1/chapter_005/testTaskBank/src/main/java/ru/agolovin/User.java
@@ -1,5 +1,7 @@
 package ru.agolovin;
 
+import java.util.Objects;
+
 /**
  * @author agolovin (agolovin@list.ru)
  * @version $Id$
@@ -28,5 +30,23 @@ public class User {
 
         this.name = name;
         this.passport = passport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return passport == user.passport
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, passport);
     }
 }
---
+++ package_1/chapter_005/testTaskBank/src/test/java/ru/agolovin/UserTest.java
@@ -0,0 +1,37 @@
+package ru.agolovin;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author agolovin (agolovin@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class UserTest {
+
+    /**
+     * Test when user1 equals user2.
+     */
+    @Test
+    public void whenUserIsSameThenResultTrue() {
+        User user1 = new User("Name1", 0);
+        User user2 = new User("Name1", 0);
+        boolean result = user1.equals(user2);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test when user1 not equals user2.
+     */
+    @Test
+    public void whenUserNotTheSameThneResultFalse() {
+        User user1 = new User("Name1", 0);
+        User user2 = new User("Name2", 1);
+        boolean result = user1.equals(user2);
+        assertThat(result, is(false));
+    }
+
+}
