061dbbbefac36db0fe4ec8c648b1051c75d6eaf9
MorpG <agolovin@list.ru>
2017-04-21 22:31:44 +0300

    add task1 in part2 chapter5 package2

--- package_2/chapter_005_pro/generic/src/test/java/ru/agolovin/SimpleArrayTest.java
+++ package_2/chapter_005_pro/generic/src/test/java/ru/agolovin/SimpleArrayTest.java
@@ -1,6 +1,9 @@
 package ru.agolovin;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author agolovin (agolovin@list.ru)
@@ -9,4 +12,43 @@ import static org.junit.Assert.*;
  */
 public class SimpleArrayTest {
 
+    /**
+     * Test add element in array.
+     */
+    @Test
+    public void whenAdd2ThenGet2() {
+        SimpleArray<Integer> ar = new SimpleArray<>(5);
+        String test = "text";
+        ar.add(1);
+        ar.add(2);
+        assertThat(ar.get(0), is(1));
+        assertThat(ar.get(1), is(2));
+    }
+
+    /**
+     * Test update element in array.
+     */
+    @Test
+    public void whenUpdateFirst() {
+        SimpleArray<Integer> ar = new SimpleArray<>(5);
+        ar.add(1);
+        ar.add(4);
+        ar.update(0, 4);
+        ar.update(1, 6);
+        assertThat(ar.get(0), is(4));
+        assertThat(ar.get(1), is(6));
+    }
+
+    /**
+     * Test delete element in array.
+     */
+    @Test
+    public void whenDelete() {
+        SimpleArray<Integer> ar = new SimpleArray<>(5);
+        ar.add(1);
+        ar.add(2);
+        ar.add(3);
+        ar.delete(1);
+        assertThat(ar.get(1), is(3));
+    }
 }
