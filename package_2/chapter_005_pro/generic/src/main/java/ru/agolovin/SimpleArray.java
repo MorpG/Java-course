c1ab6342596f269fc487ef6a5bfe4425b2bc5542
Aleksandr Golovin <agolovin@list.ru>
2017-04-21 17:17:06 +0300

    add task1 in part2 chapter5 package2

---
+++ package_2/chapter_005_pro/generic/src/main/java/ru/agolovin/SimpleArray.java
@@ -0,0 +1,25 @@
+package ru.agolovin;
+
+/**
+ * @author agolovin (agolovin@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleArray<T> {
+
+    private Object[] objects;
+
+    private int index;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    public void add(T value) {
+        this.objects[index++] = value;
+    }
+
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+}
---
+++ package_2/chapter_005_pro/generic/src/test/java/ru/agolovin/SimpleArrayTest.java
@@ -0,0 +1,12 @@
+package ru.agolovin;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author agolovin (agolovin@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleArrayTest {
+
+}
