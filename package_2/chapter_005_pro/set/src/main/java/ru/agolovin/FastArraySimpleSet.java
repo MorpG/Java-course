76a6ca27edd124c2f4bd4ff8980dde19fb518f02
MorpG <agolovin@list.ru>
2017-05-03 22:49:26 +0300

    add task3 in part4 chapter5 package2

--- package_2/chapter_005_pro/set/src/main/java/ru/agolovin/FastArraySimpleSet.java
+++ package_2/chapter_005_pro/set/src/main/java/ru/agolovin/FastArraySimpleSet.java
@@ -1,6 +1,9 @@
 package ru.agolovin;
 
+import java.util.Arrays;
+
 /**
+ * @param <E> Generic
  * @author agolovin (agolovin@list.ru)
  * @version $Id$
  * @since 0.1
@@ -9,62 +12,49 @@ public class FastArraySimpleSet<E> extends ArraySimpleSet<E> {
 
     @Override
     public void add(E e) {
-        if (contains(e) == -1) {
-            if (array.length > index) {
+        if (contains(e) < 0) {
+            int ind = getIndex();
+            if (getArray().length >= ind) {
                 riseArray();
             }
-            if (index == 0) {
-                array[index] = e;
-            } else {
-                add(getAddIndex(e), e);
-            }
-            index++;
-
+            setValue(ind, e);
+            Arrays.sort(getArray());
+            setIndex(++ind);
         }
     }
 
-    private void add(int addIndex, E element) {
-        E[] temp = (E[]) new Object[array.length + 1];
-        System.arraycopy(array, 0, temp, 0, addIndex);
-        temp[addIndex] = element;
-        System.arraycopy(array, addIndex, temp, addIndex + 1, array.length - addIndex);
-        this.array = temp;
+    /**
+     * Rise array.
+     */
+    void riseArray() {
+        setArray(Arrays.copyOf(getArray(), (getIndex() + 1)));
     }
 
-    private int getAddIndex(E e) {
-        int hashCode = e.hashCode();
-        int leftBorder = -1;
-        int rightBorder = index;
-        int middle;
-        while (leftBorder < rightBorder - 1) {
-            middle = (leftBorder + rightBorder)/ 2;
-            if (array[middle].hashCode() < hashCode) {
-                leftBorder = middle;
-            } else {
-                rightBorder = middle;
-            }
-        }
-        return rightBorder;
-    }
-
+    /**
+     * Check if element already contains in array.
+     *
+     * @param e Generic
+     * @return index element in array. Return -1 if not contains
+     */
     private int contains(E e) {
-        int i = -1;
-        if (this.array != null) {
-            int low = 0, high = this.array.length, mid;
-            while (low < high) {
-                mid = (low + high)/2; // Можно заменить на расчёт в беззнаковом типе: (low + high) >>> 1
-                if (e.equals(this.array[mid])) {
-                    i = mid;
-                    break;
-                } else {
-//                    if (e < this.array[mid]) {
-                        high = mid;
-//                    } else {
-                        low = mid + 1;
-                    }
-                }
+        int low = 0;
+        int high = getIndex() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            @SuppressWarnings("rawtypes")
+            Comparable midVal = (Comparable) getArray()[mid];
+            @SuppressWarnings("unchecked")
+            int cmp = midVal.compareTo(e);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
             }
-//        }
-        return i;
+        }
+        return -(low + 1);  // key not found.
     }
 }
--- package_2/chapter_005_pro/set/src/test/java/ru/agolovin/FastArraySimpleSetTest.java
+++ package_2/chapter_005_pro/set/src/test/java/ru/agolovin/FastArraySimpleSetTest.java
@@ -2,10 +2,12 @@ package ru.agolovin;
 
 import org.junit.Test;
 
-import java.util.Iterator;
 import java.util.Random;
 
+
 import static java.lang.String.format;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author agolovin (agolovin@list.ru)
@@ -14,37 +16,49 @@ import static java.lang.String.format;
  */
 public class FastArraySimpleSetTest {
 
+    /**
+     * Test work fasSet.
+     */
+    @Test
+    public void thenAddThenGetElement() {
+        FastArraySimpleSet<Integer> fastSet = new FastArraySimpleSet<>();
+        fastSet.add(22);
+        fastSet.add(22);
+        fastSet.add(44);
+        fastSet.add(44);
+        assertThat(fastSet.get(0), is(22));
+        assertThat(fastSet.get(1), is(44));
+    }
+
+    /**
+     * Test adding time.
+     */
     @Test
     public void testSetAddTime() {
 
-        final int iterations = 10;
+        final int iterations = 1000000;
+
+        final int bound = 900;
 
         Random rnd = new Random();
 
         long startTime = System.currentTimeMillis();
         FastArraySimpleSet<Integer> fastSet = new FastArraySimpleSet<>();
         for (int i = 0; i < iterations; i++) {
-            fastSet.add(rnd.nextInt(4));
+            fastSet.add(rnd.nextInt(bound));
         }
         long endFastTime = System.currentTimeMillis();
-        Iterator<Integer> intF = fastSet.iterator();
-        while (intF.hasNext()) {
-            System.out.println(intF.next());
-        }
+
         System.out.println(format("Fast time is: %s", endFastTime - startTime));
 
         long startTimeSet = System.currentTimeMillis();
-        ArraySimpleSet<Integer> set = new ArraySimpleSet<>(1);
+        ArraySimpleSet<Integer> set = new ArraySimpleSet<>(0);
         for (int i = 0; i < iterations; i++) {
-            set.add(rnd.nextInt(3));
+            set.add(rnd.nextInt(bound));
         }
         long endSetTime = System.currentTimeMillis();
-        Iterator<Integer> intS = set.iterator();
-        while (intS.hasNext())
-            System.out.println(intS.next());
+
         System.out.println(format("Set time is: %s", endSetTime - startTimeSet));
 
     }
-
-
 }
