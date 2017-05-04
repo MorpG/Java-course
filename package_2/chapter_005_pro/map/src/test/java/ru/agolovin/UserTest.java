acdd935dcd0c470ddc7be3d2905188396f548805
Aleksandr Golovin <agolovin@list.ru>
2017-05-04 16:33:16 +0300

    add task2 in part5 chapter5 package2

--- package_2/chapter_005_pro/map/src/test/java/ru/agolovin/UserTest.java
+++ package_2/chapter_005_pro/map/src/test/java/ru/agolovin/UserTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -15,18 +17,41 @@ import static org.junit.Assert.assertThat;
  */
 public class UserTest {
 
+    private Calendar calc = new GregorianCalendar();
+    private String name = "Name";
+    private int children = 2;
+
     /**
      * Test user class.
      */
     @Test
     public void thenAddUserThneGetInformation() {
-        Calendar calc = new GregorianCalendar();
-        String name = "Name";
-        int children = 2;
+
         User user = new User(name, children, calc);
         assertThat(user.getName(), is(name));
         assertThat(user.getChildren(), is(children));
         assertThat(user.getBirthday(), is(calc));
     }
 
+
+    /**
+     * Test add user to map w/o override equals and hashcode.
+     */
+    @Test
+    public void thenAddSameUserToMapThenItContainsOne() {
+        User user1 = new User(name, children, calc);
+        User user2 = new User(name, children, calc);
+        Map<User, Object> map = new HashMap<>();
+        Integer item = 1;
+        map.put(user1, item);
+        map.put(user2, item);
+
+        System.out.println("Map contains: ");
+        for (Map.Entry<User, Object> pair : map.entrySet()) {
+            User key = pair.getKey();
+            System.out.println(String.format("User name: %s", key.getName()));
+            System.out.println(String.format("User child: %s", key.getChildren()));
+        }
+
+    }
 }
