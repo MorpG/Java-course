ad0892d50152f1cf3151557ac93a1c0dac20ab83
MorpG <agolovin@list.ru>
2017-10-24 23:57:43 +0300

    add task1 in part4 chapter7 package2

--- package_2/multithreading/waitnotify/src/main/java/ru/agolovin/ProducerCustomer.java
+++ package_2/multithreading/waitnotify/src/main/java/ru/agolovin/ProducerCustomer.java
@@ -1,5 +1,10 @@
 package ru.agolovin;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.lang.Thread.sleep;
+
 /**
  * @author agolovin (agolovin@list.ru)
  * @version $Id$
@@ -7,5 +12,39 @@ package ru.agolovin;
  */
 
 public class ProducerCustomer {
+
+    private int quant = 5;
+
+    private boolean flag = false;
+
+    private Queue<Integer> queue = new LinkedBlockingQueue<>();
+
+    public void init() {
+
+    }
+
+    private Thread producer() {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < quant; i++) {
+                synchronized (queue) {
+                    queue.add(i++);
+                    queue.notify();
+                    System.out.println(String.format(
+                            "Thread %s added %s", Thread.currentThread().getId(), i));
+                }
+                if ( i == (quant - 1)) {
+                    flag = true;
+                }
+                try {
+                    Thread.sleep(10);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return thread;
+    }
+
 
 }
