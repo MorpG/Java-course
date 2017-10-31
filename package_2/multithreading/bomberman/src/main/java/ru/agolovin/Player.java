390e155ce873697167b7cdd71835e4b9abab6181
MorpG <agolovin@list.ru>
2017-10-31 22:59:08 +0300

    add test task in part6 chapter7 package2

---
+++ package_2/multithreading/bomberman/src/main/java/ru/agolovin/Player.java
@@ -0,0 +1,21 @@
+package ru.agolovin;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author agolovin (agolovin@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Player  implements Runnable{
+
+    private ReentrantLock[][] board;
+
+    private boolean flag;
+
+    @Override
+    public void run() {
+
+    }
+}
