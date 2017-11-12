4a4c7e02d0b033c9e3f59c744a2cf4b355fec691
MorpG <agolovin@list.ru>
2017-11-12 21:14:07 +0300

    add test task in part6 chapter7 package2

--- package_2/multithreading/bomberman/src/main/java/ru/agolovin/Bomberman.java
+++ package_2/multithreading/bomberman/src/main/java/ru/agolovin/Bomberman.java
@@ -1,5 +1,7 @@
 package ru.agolovin;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author agolovin (agolovin@list.ru)
  * @version $Id$
@@ -8,4 +10,26 @@ package ru.agolovin;
 
 public class Bomberman {
 
+    private final ReentrantLock[][] board;
+    private final int size;
+    private final int bots;
+    private final Player[] players;
+    private final Boolean cellStatus[][];
+
+    Bomberman(int size, int bots) {
+        this.size = size;
+        this.board = new ReentrantLock[size][size];
+        this.bots = bots;
+        this.players = new Player[bots];
+        this.cellStatus = new Boolean[size][size];
+    }
+
+    private void prepareBoard() {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                this.board[i][j] = new ReentrantLock();
+                this.cellStatus[i][j] = false;
+            }
+        }
+    }
 }
--- package_2/multithreading/bomberman/src/main/java/ru/agolovin/Player.java
+++ package_2/multithreading/bomberman/src/main/java/ru/agolovin/Player.java
@@ -8,14 +8,30 @@ import java.util.concurrent.locks.ReentrantLock;
  * @since 0.1
  */
 
-public class Player  implements Runnable{
+public class Player implements Runnable {
 
     private ReentrantLock[][] board;
+
+    private int xCell;
+    private int yCell;
+
+    private ReentrantLock lock;
 
     private boolean flag;
 
     @Override
     public void run() {
 
+    }
+
+    private boolean isCorrectMove(int xCell, int yCell) {
+        boolean result = false;
+
+        if ((xCell >= 0) && xCell < this.board.length
+                && (yCell >= 0 && yCell < this.board.length)) {
+            result = true;
+        }
+
+        return result;
     }
 }
