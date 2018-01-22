f7c982b260864a029039b2ae9396749455a706ac
MorpG <agolovin@list.ru>
2018-01-23 00:20:40 +0300

    add additional test task in part6 chapter7 package2

---
+++ package_2/multithreading/elevator/src/main/java/ru/agolovin/UserInput.java
@@ -0,0 +1,43 @@
+package ru.agolovin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserInput implements Runnable {
+
+    private int maxFloor;
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    @Override
+    public void run() {
+        ask("S");
+    }
+
+    public int ask(String s) {
+        boolean flag = false;
+        int level = -1;
+        System.out.println(s);
+        do {
+            try {
+                String line = reader.readLine();
+                try {
+                    level = Integer.parseInt(line);
+                } catch (NumberFormatException e) {
+                    System.out.println("Ошибка ввода");
+                    continue;
+                }
+                if (level > 0 && level <= maxFloor) {
+                    flag = true;
+                } else {
+                    System.out.println("Несущестующий этаж");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (flag);
+        return level;
+    }
+
+
+}
